6bf45b4402aa7b155388549c3de262a68f0fa595
Arulasok <31365024+Arulasok@users.noreply.github.com>
2020-10-12 14:44:44 +0530

    Add files via upload

--- src/Components/parentChild.vue
+++ src/Components/parentChild.vue
@@ -1,1 +1,24 @@
+<template>
+  <div>{{ msg }}</div>
+  <button v-if="msg" @click="sendMessage">Ok</button>
+  <slot><p>This is child</p></slot>
+</template>
 
+<script>
+export default {
+  name: "parentChild",
+  props: {
+    msg: String,
+  },
+  setup(props, ctx) {
+    console.log(props);
+    function sendMessage() {
+      ctx.emit("finished");
+    }
+    return {
+      props,
+      sendMessage,
+    };
+  },
+};
+</script>
