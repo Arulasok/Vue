8493d65485b6cead6a3e995cc61d84624750ff78
Arulasok <31365024+Arulasok@users.noreply.github.com>
2020-10-12 14:45:49 +0530

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Counter from '../views/Counter.vue'
+import twoWay from '../views/twoWay.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/counter',
+    name: 'Counter',
+    component: Counter
+  },
+  {
+    path: '/twoWay',
+    name: 'twoWay',
+    component: twoWay
+  },
+]
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
